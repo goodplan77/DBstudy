5bb534b742a9db59f412e0a7db8d8a2c9d57101d
goodplan77 <goodplan77@naver.com>
2024-05-01 17:49:00 +0900

    2024.05.01 SQL Class file (DQL , DDL)

--- 05_DQL(SELECT)_SUBQUERY.sql
+++ 05_DQL(SELECT)_SUBQUERY.sql
@@ -13,7 +13,7 @@
 -- 1) 노옹철사원과 같은 부서인 사원들 정보를 조회
 SELECT DEPT_CODE
 FROM EMPLOYEE
-WHERE EMP_NAME = '노옹철'
+WHERE EMP_NAME = '노옹철';
 
 SELECT *
 FROM EMPLOYEE
@@ -78,13 +78,13 @@ WHERE SALARY > (SELECT SALARY FROM EMPLOYEE WHERE EMP_NAME='노옹철');
 -- 1) 각 부서별 급여의 합을 구한후 , 최대값 찾기
 SELECT MAX(SUM(SALARY))
 FROM EMPLOYEE
-GROUP BY DEPT_CODE
+GROUP BY DEPT_CODE;
 
 -- 2) 메인 쿼리문 작성
 SELECT DEPT_CODE , DEPT_TITLE , SUM(SALARY) 급여의합
 FROM EMPLOYEE
 LEFT JOIN DEPARTMENT ON DEPT_CODE=DEPT_ID
-GROUP BY DEPT_CODE , DEPT_TITLE;
+GROUP BY DEPT_CODE , DEPT_TITLE
 HAVING SUM(SALARY) = (SELECT MAX(SUM(SALARY)) FROM EMPLOYEE GROUP BY DEPT_CODE);
 
 /*
@@ -119,7 +119,7 @@ WHERE JOB_CODE IN (SELECT JOB_CODE FROM EMPLOYEE WHERE EMP_NAME IN ('이오리',
 SELECT SALARY
 FROM EMPLOYEE
 JOIN JOB USING(JOB_CODE)
-WHERE JOB_NAME = '과장'
+WHERE JOB_NAME = '과장';
 
 -- 2) 위의 급여들보다 하나라도 더 높은 급여를 받는 직원들 조회
 SELECT EMP_ID , EMP_NAME , JOB_NAME , SALARY
@@ -148,3 +148,166 @@ WHERE JOB_NAME = '과장' AND SALARY >= ALL (
     서브쿼리 조회 결과가 단일행이지만 , 나열된 칼럼의 갯수가 여러개인경우.
 */
 -- 하이유 사원과 같은 부서코드 이면서 같은 직급코드에 해당되는 사원들 조회 (사원명 , 부서코드 , 직급코드 , 고용날짜)
+SELECT DEPT_CODE , JOB_CODE
+FROM EMPLOYEE
+WHERE EMP_NAME = '하이유';
+
+SELECT EMP_NAME , DEPT_CODE , JOB_CODE , HIRE_DATE
+FROM EMPLOYEE
+WHERE DEPT_CODE = 'D5' AND JOB_CODE = 'J5';
+
+-- 다중열 서브쿼리 (비교할 값의 순서를 맞춰줘야함.)
+-- (비교대상칼럼1 , 비교대상칼럼2 , ...) = (비교할값1 , 비교할값2 , ...)
+-- 서브쿼리 형태로 제공 해줘야함 (리터럴값 제시 불가)
+SELECT EMP_NAME , DEPT_CODE , JOB_CODE , HIRE_DATE
+FROM EMPLOYEE
+WHERE (DEPT_CODE , JOB_CODE) = (SELECT DEPT_CODE , JOB_CODE
+FROM EMPLOYEE
+WHERE EMP_NAME = '하이유');
+
+-- 박나라 사원과 같은 직급 코드 이면서 같은 사수 사번을 가진 사원들의 사번 , 이름 , 직급 코드 , 사수 사번
+SELECT EMP_ID , EMP_NAME , JOB_CODE , MANAGER_ID
+FROM EMPLOYEE
+WHERE (JOB_CODE , MANAGER_ID) = (
+SELECT JOB_CODE , MANAGER_ID
+FROM EMPLOYEE
+WHERE EMP_NAME = '박나라'
+);
+
+/*
+    4. 다중행 다중열 서브쿼리
+    서브 쿼리 조회 결과가 여러행 여러 컬럼일 경우
+*/
+
+-- 각 직급별로 최소 급여를 받는 사원들 조회 (사번 , 이름 , 직급 코드 , 급여)
+SELECT JOB_CODE , MIN(SALARY)
+FROM EMPLOYEE
+GROUP BY JOB_CODE;
+
+SELECT EMP_ID , EMP_NAME , JOB_CODE , SALARY
+FROM EMPLOYEE
+/*
+WHERE (JOB_CODE , SALARY) = ('J1' , 8000000)
+OR (JOB_CODE , SALARY) = ('J2' , 37000000)
+...
+(JOB_CODE , SALARY) IN (('J1' , 8000000) , ('J2' , 37000000) ...)
+*/
+WHERE (JOB_CODE , SALARY) IN (
+SELECT JOB_CODE , MIN(SALARY)
+FROM EMPLOYEE
+GROUP BY JOB_CODE
+);
+
+-- 각 부서별 최고 급여를 받는 사원 , 부서가 없을 경우 NULL값이 아닌 '부서없음' 으로 조회되게끔하시오
+-- 사번 , 이름 , 부서코드 , 급여
+SELECT EMP_ID , EMP_NAME , NVL(DEPT_CODE , '부서없음') , SALARY
+FROM EMPLOYEE
+WHERE (NVL(DEPT_CODE,'부서없음') , SALARY) IN (SELECT NVL(DEPT_CODE, '부서없음') , MAX(SALARY)
+FROM EMPLOYEE
+GROUP BY DEPT_CODE);
+--------------------------------------------
+/*
+    5. 스칼라 서브 쿼리
+    - 단일행 (단일열) 서브쿼리의 일종으로 SELECT 절에서 사용되는 서브쿼리를 지칭함.
+        SELECT 절이 실행 될 때마다 서브쿼리문이 실행되면서 조회결과값을 반환.
+    - 현재 조회된 행의 "칼럼값"을 서브쿼리 내에서 사용가능함.★
+    - 매 행마다 실행되기 때문에 대규모 데이터 처리시 효율이 좋지 못함
+*/
+
+-- 직원번호 , 직원명 , 부서명을 조회
+SELECT
+    EMP_ID,
+    EMP_NAME,
+    DEPT_TITLE
+FROM EMPLOYEE
+JOIN DEPARTMENT ON DEPT_CODE = DEPT_ID;
+
+SELECT
+    EMP_ID,
+    EMP_NAME,
+    (SELECT DEPT_TITLE FROM DEPARTMENT WHERE DEPT_ID = DEPT_CODE) AS DEPT_TITLE
+FROM EMPLOYEE;
+
+-- 사번 , 사원명 , 부서코드 , 직급명 조회
+SELECT
+    EMP_NO,
+    EMP_NAME,
+    DEPT_CODE 부서코드,
+    (SELECT JOB_NAME FROM JOB J WHERE J.JOB_CODE = E.JOB_CODE) AS JOB_NAME
+FROM EMPLOYEE E;
+
+-- LOCATION 테이블에서 지역코드와 , 국가명 (스칼라 서브쿼리 활용) 조회하기
+SELECT
+    LOCAL_CODE,
+    (SELECT NATIONAL_NAME FROM NATIONAL N WHERE L.NATIONAL_CODE = N.NATIONAL_CODE) AS NATIONAL_NAME
+FROM LOCATION L;
+
+-------------------------------------------------------------
+/*
+    6. 인라인 뷰
+    - FROM 절에서 사용되는 서브 쿼리
+    
+    서브 쿼리를 수행한 결과(RESULT SET)을 테이블 대신 사용
+*/
+-- 보너스 포함 연봉이 3000만원 이상인 사원들의 사번 , 이름 , 보너스 포함 연봉 , 부서코드 조회
+SELECT *
+FROM (SELECT EMP_ID,
+            EMP_NAME,
+            SALARY * (1 + NVL(BONUS,0)) * 12 AS "보너스 포함 연봉",
+             DEPT_CODE
+     FROM EMPLOYEE
+     -- WHERE "보너스 포함 연봉" >= 3000000; -- 부적합한 식별자 (조회순서상 SELECT 가 더 늦게 실행)
+    )
+WHERE "보너스 포함 연봉" >= 3000000; -- 보너스 포함 연봉이 먼저 데이터로 생성된 후 칼럼을 사용하였으므로 문제 없음
+-- FROM -> WHERE -> SELECT
+
+-- 인라인뷰를 많이 사용하는 예시
+-- TOP-N분석 : 데이터베이스상에 있는 자료들중 최상위 N개의 자료를 보기 위한 기능
+
+-- 전 직원중 급여가 가장 높은 상위 5명을 조회(순위 , 사원명 , 급여)
+SELECT
+    ROW_NUMBER() OVER (ORDER BY SALARY DESC) "순위",
+    EMP_NAME,
+    SALARY
+FROM EMPLOYEE;
+
+SELECT *
+FROM (SELECT
+    ROW_NUMBER() OVER (ORDER BY SALARY DESC) "순위",
+    EMP_ID,
+    SALARY
+    FROM EMPLOYEE)
+WHERE "순위" <= 5;
+
+-- * ROWNUM : 오라클에서 제공해주는 칼럼 , 조회둔 순서대로 1부터 순번을 부여해주는 컬럼
+SELECT EMP_NAME , SALARY , ROWNUM
+FROM EMPLOYEE
+WHERE ROWNUM <=5;
+-- ORDER BY SALARY DESC; 순서 엉킴
+
+-- 1) ORDER BY로 테이블 먼저 정렬한후, ROWNUM 순번을 통해 급여가 높은 5명만 추출
+SELECT EMP_NAME , SALARY , ROWNUM
+FROM (SELECT * FROM EMPLOYEE ORDER BY SALARY DESC)
+WHERE ROWNUM <=5;
+
+SELECT *
+FROM (
+    SELECT EMP_NAME , SALARY , ROWNUM AS R
+    FROM (
+    SELECT * FROM EMPLOYEE ORDER BY SALARY DESC
+    )
+)
+WHERE R >= 5 AND R <= 10;
+------------------------------------------
+-- 각 부서별 평균 급여가 높은 3개의 부서의 부서코드 , 평균 급여 조회
+SELECT DEPT_CODE , AVG(SALARY)
+FROM EMPLOYEE
+GROUP BY DEPT_CODE
+ORDER BY AVG(SALARY) DESC;
+
+SELECT DEPT_CODE , 평균급여
+FROM (SELECT DEPT_CODE , ROUND(AVG(SALARY)) AS 평균급여
+FROM EMPLOYEE
+GROUP BY DEPT_CODE
+ORDER BY 2 DESC) R
+WHERE ROWNUM <=3;
---
+++ 06_DDL(CREATE).sql
@@ -0,0 +1,268 @@
+/*
+    DDL(DATA DEFINITION LANGUAGE) 데이터 정의 언어
+    
+    오라클에서 제공하는 객체를 새롭게 만들고 , 구조를 변경하고 , 구조를 삭제 할 수 있는 명령문.
+    즉 , 구조 자체를 정의하는 언어로 DB관리자 , 설계자가 사용
+    
+    객체란 DATABASE를 이루는 구조물들.
+    테이블 , 인덱스 , 사용자 , 함수 , 뷰 , 시퀀스
+*/
+
+/*
+    <CREATE TABLE>
+    
+    테이블 : 행과 열로 구성되어 있는 가장 기본적인 데이터 베이스 객체 종류 중 하나
+            모든 데이터는 테이블을 통해서 저장됨. (즉 , 데이터를 조작하고자 한다면 무조건 테이블을 만들어야함)
+            
+    [표현법]
+    CREATE TABLE 테이블명 (
+        컬럼명 자료형 , 
+        ...
+    );
+    
+    <자료형>
+    - 문자 (CHAR(크기)/VARCHAR2(크기)) : 크기 BYTE수로 지정 , 글자길이 CHAR로 지정.
+                                    (숫자 , 영문자 , 특수문자 => 1BYTE)
+                                    (한글 3BYTE)
+                                    (100 BYTE -> 100BYTE 까지 추가 / 100 CHAR => 100글자 까지 추가)
+                        CHAR(크기) : 고정 크기 (적은 값이 들어와도 공백으로 채워서 할당한 크기 유지)
+                                    2000BYTE
+                                    주로 들어올 값의 글자수가 정해져 있을 경우 사용.
+                                    (성별 , 주민번호)
+                        VARCHAR2(크기) : 가변길이 (적은 값이 들어온 경우 그 값만큼의 저장공간을 사용)
+                                        4000BYTE
+                                        들어올 값의 글자수가 정해지지 않을 경우 사용
+    - 숫자 (NUMBER(양수자리,실수자리)) : 정수/실수 상관없이 NUMBER
+    
+    - 날짜 (DATE) : 년/월/일/시/분/초 형식으로 시간지정    
+*/
+
+-->> 회원관리 테이블 (아이디 , 비밀번호 , 이름 , 생년월일)를 담기위한 MEMBER 테이블
+CREATE TABLE MEMBER(
+    MEMBER_ID VARCHAR2(20),
+    MEMBERPWD/*memberPwd 대소문자 구분안함*/ VARCHAR2(20),
+    MEMBERNAME VARCHAR2(20),
+    BIRTHDAY DATE
+);
+DROP TABLE MEMBER;
+SELECT * FROM MEMBER;
+
+-- 데이터 딕셔너리(사전) : 다양한 객체들의 정보를 저장하고 있는 시스템 테이블
+SELECT * FROM USER_TABLES;
+-- USER_TABLES : 현재 사용자 계정이 가지고 있는 테이블들의 구조를 확인 할 수 있는 데이터 사전
+
+SELECT * FROM USER_TAB_COLUMNS;
+-- USER_TAB_COLUMNS : 현재 사용자 계정이 가지고 있는 테이블들의 전반적인 구조를 확인 할 수있는 데이터 사전.
+
+/*
+    컬럼에 주석달기(컬럼에 대한 설명)
+    
+    [표현법]
+    COMMENT ON COLUMN 테이블명.컬럼명 IS '주석내용';
+*/
+COMMENT ON COLUMN MEMBER.MEMBER_ID IS '회원아이디';
+COMMENT ON COLUMN MEMBER.MEMBERPWD IS '비밀번호';
+COMMENT ON COLUMN MEMBER.MEMBERNAME IS '이름';
+COMMENT ON COLUMN MEMBER.BIRTHDAY IS '생년월일';
+
+
+-- 참고 : INSERT (데이터추가구문) => DML문
+INSERT INTO MEMBER VALUES('user01','pass01','홍길동','80/10/06');
+INSERT INTO MEMBER VALUES('user02','pass02','김길동','80/10/06');
+INSERT INTO MEMBER VALUES('user03','pass03','강길동',SYSDATE);
+
+SELECT * FROM MEMBER;
+
+INSERT INTO MEMBER VALUES(NULL,NULL,'강길동',SYSDATE);
+INSERT INTO MEMBER VALUES('user01','pass01','홍길동','80/10/06');
+
+/*
+    -- 제약조건 : 테이블을 만들때 지정해주는 방법
+    
+    <제약조건 CONSTRAINTS>
+    - 원하는 데이터값만 유지하기 위해서 특정 "컬럼" 단위로 설정하는 제약 (데이터의 무결성 목적)
+    - 제약조건이 부여된 칼럼에 들어올 데이터에 문제가 있는지 없는지 자동으로 검사를 해줌.
+    
+    - 종류 : NOT NULL , UNIQUE , CHECK , PRIMARY KEY , FOREIGN KEY
+    
+    - 테이블 생성시 컬럼에 제약조건을 부여 하는 방식 : 컬럼레벨 방식 / 테이블레벨 방식
+*/
+
+/*
+    1. NOT NULL 제약조건
+        해당 컬럼에는 반드시 값이 존재해야만 할 경우 사용
+        =>  NULL 값이 절대 들어와서는 안되는 컬럼에 부여하는 제약조건
+            데이터의 삽입 / 수정시 NULL 값을 허용하지 않도록 제한하는 제약조건
+        주의사항 : 컬럼레벨방식으로만 제약조건 부여 가능
+*/
+
+-- NOT NULL 제약조건을 설정한 테이블 만들기
+-- 컬럼레벨 방식 : 컬럼명 자료형 제약조건
+CREATE TABLE MEM_NOTNULL (
+    MEM_NO NUMBER NOT NULL,
+    MEM_ID VARCHAR(20) NOT NULL,
+    MEM_PWD VARCHAR2(20) NOT NULL,
+    MEM_NAME VARCHAR2(20) NOT NULL,
+    GENDER CHAR(3), 
+    PHONE VARCHAR2(15),
+    EMAIL VARCHAR2(30)
+);
+DROP TABLE MEM_NOTNULL;
+
+INSERT INTO MEM_NOTNULL VALUES (
+    1 , 'user01' , 'pass01' , '홍길동' , '남' , '010-1111-1111' , 'aaa@naver.com'
+);
+
+SELECT * FROM MEM_NOTNULL;
+
+INSERT INTO MEM_NOTNULL VALUES (
+    2 , NULL , 'pass01' , '홍길동' , '남' , '010-1111-1111' , 'aaa@naver.com'
+);
+-- DDL 계정에 MEM_NOTNULL 테이블에 MEM_ID , MEM_NO등 NOT NULL 제약조건이 부여되어있는 컬럼에는 NULL이 들어 갈 수 없음.
+
+
+/*
+    2. UNIQUE 제약조건
+        컬럼에 중복값을 제한하는 제약조건.
+        삽입 / 수정시 기존에 해당 컬럼값에 중복값이 있는 경우 추가나 수정이 되지 않게 제약.
+*/
+CREATE TABLE MEM_UNIQUE (
+    MEM_NO NUMBER NOT NULL UNIQUE,
+    MEM_ID VARCHAR(20) NOT NULL UNIQUE,
+    MEM_PWD VARCHAR2(20) NOT NULL,
+    MEM_NAME VARCHAR2(20) NOT NULL,
+    GENDER CHAR(3), 
+    PHONE VARCHAR2(15),
+    EMAIL VARCHAR2(30)
+);
+DROP TABLE MEM_UNIQUE;
+
+CREATE TABLE MEM_UNIQUE (
+    MEM_NO NUMBER NOT NULL ,
+    MEM_ID VARCHAR(20) NOT NULL ,
+    MEM_PWD VARCHAR2(20) NOT NULL,
+    MEM_NAME VARCHAR2(20) NOT NULL,
+    GENDER CHAR(3), 
+    PHONE VARCHAR2(15),
+    EMAIL VARCHAR2(30),
+    
+    UNIQUE (MEM_NO , MEM_ID) -- 테이블 레벨 방식 
+);
+
+INSERT INTO MEM_UNIQUE
+VALUES (1,'user01','pass01','홍길동',NULL,NULL,NULL);
+
+INSERT INTO MEM_UNIQUE
+VALUES (2,'user01','pass01','홍길동',NULL,NULL,NULL);
+
+SELECT * FROM USER_CONSTRAINTS;
+
+/*
+    * 제약조건 부여시 제약조건의 이름도 함께 지정하는 방법.
+    > 컬럼레벨방식
+    컬럼명 자료형 제약조건 1 제약조건2
+    컬럼명 자료형 CONSTRAINT 제약조건명 제약조건
+    
+    > 테이블레벨방식
+    ...
+    CONSTRAINT 제약조건명 제약조건 (칼럼명)
+*/
+
+DROP TABLE MEM_UNIQUE;
+
+CREATE TABLE MEM_UNIQUE (
+    MEM_NO NUMBER NOT NULL CONSTRAINT MEM_UNIQUE_UQ_MEM_NO UNIQUE ,
+    MEM_ID VARCHAR(20) NOT NULL ,
+    MEM_PWD VARCHAR2(20) NOT NULL,
+    MEM_NAME VARCHAR2(20) NOT NULL,
+    GENDER CHAR(3), 
+    PHONE VARCHAR2(15),
+    EMAIL VARCHAR2(30),
+    
+    CONSTRAINT MEM_UNIQUE_UQ_MEM_ID UNIQUE (MEM_ID)
+);
+
+INSERT INTO MEM_UNIQUE
+VALUES (1,'user01','pass01','홍길동',NULL,NULL,NULL);
+
+INSERT INTO MEM_UNIQUE
+VALUES (2,'user01','pass01','홍길동',NULL,NULL,NULL);
+
+/*
+    3. CHECK 제약 조건
+        칼럼에 기록 될 수 있는 값에 대한 조건을 설정할때 사용
+        EX) 성별 에는 '남' 혹은 '여'만 들어오게 끔 할때 사용
+        
+        [표현식]
+        CHECK(조건식)
+*/
+
+CREATE TABLE MEM_CHECK (
+    MEM_NO NUMBER NOT NULL CONSTRAINT MEM_UNIQUE_UQ_MEM_NO UNIQUE ,
+    MEM_ID VARCHAR(20) NOT NULL ,
+    MEM_PWD VARCHAR2(20) NOT NULL,
+    MEM_NAME VARCHAR2(20) NOT NULL,
+    GENDER CHAR(3) NOT NULL CHECK(GENDER IN ('남','여')), 
+    PHONE VARCHAR2(15),
+    EMAIL VARCHAR2(30),
+    
+    CONSTRAINT MEM_UNIQUE_UQ_MEM_ID UNIQUE (MEM_ID)
+);
+
+INSERT INTO MEM_CHECK
+VALUES (1,'user01','pass01','홍길동',NULL,NULL,NULL);
+-- NOT NULL 제약조건을 추가해야 NULL 값 삽입을 방지 할 수있음.
+
+INSERT INTO MEM_CHECK
+VALUES (2,'user02','pass01','홍길동','남',NULL,NULL);
+-- 정상
+
+INSERT INTO MEM_CHECK
+VALUES (3,'user03','pass01','홍길동','국',NULL,NULL);
+-- CHECK 제약 조건 위배
+
+SELECT * FROM MEM_CHECK;
+
+/*
+    *   DEFAULT 설정
+        특정 칼럼에 들어올 값에 대한 기본값 설정. (제약조건은 아님)
+        
+        회원가입일 칼럼에 회원정보가 삽입되는 순간 현재시간정보를 기록.
+*/
+
+DROP TABLE MEM_CHECK;
+
+CREATE TABLE MEM_CHECK (
+    MEM_NO NUMBER NOT NULL CONSTRAINT MEM_UNIQUE_UQ_MEM_NO UNIQUE ,
+    MEM_ID VARCHAR(20) NOT NULL ,
+    MEM_PWD VARCHAR2(20) NOT NULL,
+    MEM_NAME VARCHAR2(20) NOT NULL,
+    GENDER CHAR(3) CHECK(GENDER IN ('남','여')), 
+    PHONE VARCHAR2(15),
+    EMAIL VARCHAR2(30),
+    MEM_DATE DATE DEFAULT SYSDATE,
+    
+    CONSTRAINT MEM_UNIQUE_UQ_MEM_ID UNIQUE (MEM_ID)
+);
+
+INSERT INTO MEM_CHECK
+VALUES (1,'user01','pass01','홍길동',NULL,NULL,NULL,NULL);
+
+INSERT INTO MEM_CHECK
+VALUES (2,'user02','pass01','홍길동',NULL,NULL,NULL,DEFAULT);
+
+INSERT INTO MEM_CHECK(MEM_NO , MEM_ID , MEM_PWD , MEM_NAME)
+VALUES (3,'user03','pass03','김길동');
+
+SELECT * FROM MEM_CHECK;
+
+DROP TABLE MEM_CHECK;
+
+/*
+    4. PRIMARY KEY(기본키) 제약조건
+        테이블에서 각 행들의 정보를 유일하게 식별할 수 있는 컬럼에 부여하는 제약조건
+        => 각 행들을 구분 할 수 있는 식별자의 역할.
+        EX) 사번 , 회원번호 , 부서아이디 , 직급코드...
+        => 식별자의 조건 : 중복X , 값이 반드시 존재해야함 -> UNIQUE + NOT NULL
+        기본키 제약조건은 테이블에 한개만 지정 가능함.
+*/
